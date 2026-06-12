0670a9492ce5f6c1091c3fcb3f98b8870d7937dc
oraclematrix <ehsanmohamadipoor@gmail.com>
2026-06-12 11:27:49 +0330

    feat(core): add local storage key constant for bookmarked news

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
-  static const String apiKey = "YOUR_API_KEY";
+  static const String apiKey = "ae70f501447e4095826bee9bae10ff45";
   static const String baseUrl = 'https://newsapi.org/v2/';
+  static const String getBookmarkedNewsKey = "bookmarked_news_key";
 }
