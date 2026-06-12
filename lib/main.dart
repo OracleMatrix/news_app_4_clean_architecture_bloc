ba7c26b561e6da1cb87fb3d701390f2109e7330f
oraclematrix <ehsanmohamadipoor@gmail.com>
2026-06-12 11:27:49 +0330

    feat(core): initialize GetStorage in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:news_app_4_clean_architecture_bloc/core/router/app_pages.dart';
 import 'package:news_app_4_clean_architecture_bloc/di.dart';
 import 'package:news_app_4_clean_architecture_bloc/features/feature_home/presentation/bloc/home_bloc.dart';
@@ -7,6 +8,7 @@ import 'package:news_app_4_clean_architecture_bloc/features/feature_home/present
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
+  await GetStorage.init();
 
   runApp(
     MultiBlocProvider(
