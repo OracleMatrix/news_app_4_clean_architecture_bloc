a776de99296a66666bc528902f88eafdd07bea1f
oraclematrix <ehsanmohamadipoor@gmail.com>
2026-06-12 11:27:49 +0330

    feat(home): add BookmarkedNewsMapper extension to GetNewsEntity

--- lib/features/feature_home/domain/entities/get_news_entity.dart
+++ lib/features/feature_home/domain/entities/get_news_entity.dart
@@ -1,22 +1,15 @@
 import 'package:equatable/equatable.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/entity/bookmarked_news_entity.dart';
 
 class GetNewsEntity extends Equatable {
   final String? status;
   final int? totalResults;
   final List<ArticleEntity>? articles;
 
-  const GetNewsEntity({
-    this.status,
-    this.totalResults,
-    this.articles,
-  });
+  const GetNewsEntity({this.status, this.totalResults, this.articles});
 
   @override
-  List<Object?> get props => [
-        status,
-        totalResults,
-        articles,
-      ];
+  List<Object?> get props => [status, totalResults, articles];
 }
 
 class ArticleEntity extends Equatable {
@@ -42,29 +35,38 @@ class ArticleEntity extends Equatable {
 
   @override
   List<Object?> get props => [
-        source,
-        author,
-        title,
-        description,
-        url,
-        urlToImage,
-        publishedAt,
-        content,
-      ];
+    source,
+    author,
+    title,
+    description,
+    url,
+    urlToImage,
+    publishedAt,
+    content,
+  ];
 }
 
 class SourceEntity extends Equatable {
   final String? id;
   final String? name;
 
-  const SourceEntity({
-    this.id,
-    this.name,
-  });
+  const SourceEntity({this.id, this.name});
 
   @override
-  List<Object?> get props => [
-        id,
-        name,
-      ];
+  List<Object?> get props => [id, name];
+}
+
+extension BookmarkedNewsMapper on BookMarkedNewsEntity {
+  ArticleEntity toArticleEntity() {
+    return ArticleEntity(
+      source: SourceEntity(id: '', name: sourceName),
+      author: author,
+      title: title,
+      description: description,
+      url: url,
+      urlToImage: urlToImage,
+      publishedAt: publishedAt,
+      content: content,
+    );
+  }
 }
