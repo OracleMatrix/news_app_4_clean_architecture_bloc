a90303a9e23e6078e8a2d0c283febcb8e010d42d
oraclematrix <ehsanmohamadipoor@gmail.com>
2026-06-12 11:27:49 +0330

    feat(core): inject dependencies for bookmarked news feature

--- lib/di.dart
+++ lib/di.dart
@@ -1,5 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/data/data_source/get_bookmarked_news_local_source.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/data/repository/bookmarked_news_repository_impl.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/repository/bookmarked_news_repository.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/usecases/add_news_to_bookmark_usecase.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/usecases/check_is_news_bookmarked_usecase.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/usecases/get_bookmarked_news_usecase.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/domain/usecases/remove_bookmarked_news_usecase.dart';
+import 'package:news_app_4_clean_architecture_bloc/features/feature_bookmarked_news/presentation/bloc/book_marked_news_bloc.dart';
 import 'package:news_app_4_clean_architecture_bloc/features/feature_home/data/data_source/get_news_request.dart';
 import 'package:news_app_4_clean_architecture_bloc/features/feature_home/data/repositories/get_news_repository_impl.dart';
 import 'package:news_app_4_clean_architecture_bloc/features/feature_home/domain/repositories/get_news_repository.dart';
@@ -27,5 +35,35 @@ Future setup() async {
   // News Details Feature
   di.registerSingleton<NewsDetailsRepository>(NewsDetailsRepositoryImpl());
   di.registerSingleton<ShareNewsUseCase>(ShareNewsUseCase(repository: di()));
-  di.registerFactory<NewsDetailsBloc>(() => NewsDetailsBloc(shareNewsUseCase: di()));
+  di.registerFactory<NewsDetailsBloc>(
+    () => NewsDetailsBloc(shareNewsUseCase: di()),
+  );
+
+  // Bookmarked News Feature
+  di.registerSingleton<GetBookmarkedNewsLocalSource>(
+    GetBookmarkedNewsLocalSourceImpl(),
+  );
+  di.registerSingleton<BookmarkedNewsRepository>(
+    BookmarkedNewsRepositoryImpl(getBookmarkedNewsLocalSource: di()),
+  );
+  di.registerSingleton<GetBookmarkedNewsUsecase>(
+    GetBookmarkedNewsUsecase(bookmarkedNewsRepository: di()),
+  );
+  di.registerSingleton<AddNewsToBookmarkUsecase>(
+    AddNewsToBookmarkUsecase(bookmarkedNewsRepository: di()),
+  );
+  di.registerSingleton<RemoveBookmarkedNewsUsecase>(
+    RemoveBookmarkedNewsUsecase(bookmarkedNewsRepository: di()),
+  );
+  di.registerSingleton<CheckIsNewsBookmarkedUsecase>(
+    CheckIsNewsBookmarkedUsecase(bookmarkedNewsRepository: di()),
+  );
+  di.registerFactory<BookMarkedNewsBloc>(
+    () => BookMarkedNewsBloc(
+      getBookmarkedNewsUsecase: di(),
+      addNewsToBookmarkUsecase: di(),
+      removeBookmarkedNewsUsecase: di(),
+      isNewsBookmarkedUsecase: di(),
+    ),
+  );
 }
